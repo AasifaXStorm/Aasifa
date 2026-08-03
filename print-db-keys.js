9c6fb3b2f5e1eb7e44540e691ca53c6678a80397
aasifa <ismailhatab88@gmail.com>
2026-08-03 13:27:54 +0300

    Cleanup temporary test scripts

--- print-db-keys.js
+++
@@ -1,2 +0,0 @@
-console.log("Checking DB variables in process.env...");
-console.log(Object.keys(process.env).filter(k => k.includes('DATABASE') || k.includes('POSTGRES') || k.includes('PG') || k.includes('CONN')));
